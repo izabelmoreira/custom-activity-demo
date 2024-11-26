04244f5b0491d3a44250311ea956e628bd314b2a
Izabel Moreira <izabelassuncaom@gmail.com>
2024-11-26 19:11:06 -0300

    Initial commit

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -4,28 +4,29 @@ module.exports = function configJSON(req) {
     metaData: {
       // the location of our icon file
       icon: `images/icon.svg`,
-      category: 'customer'
+      category: 'customer',
     },
     // For Custom Activity this must say, "REST"
     type: 'REST',
     lang: {
       'en-US': {
         name: 'Discount Code',
-        description: 'Issue a discount code to contact them to entice them to buy something.'
-      }
+        description:
+          'Issue a discount code to contact them to entice them to buy something.',
+      },
     },
     arguments: {
       execute: {
         // See: https://developer.salesforce.com/docs/atlas.en-us.mc-apis.meta/mc-apis/how-data-binding-works.htm
         inArguments: [
           {
-            discount: 10
-          }
+            discount: 10,
+          },
         ],
         outArguments: [],
         // Fill in the host with the host that this is running on.
         // It must run under HTTPS
-        url: `https://${req.headers.host}/modules/discount-code/execute`,
+        url: `https://custom-activity-demo-0b2f5160126d.herokuapp.com/modules/discount-code/execute`,
         // The amount of time we want Journey Builder to wait before cancel the request. Default is 60000, Minimal is 1000
         timeout: 10000,
         // how many retrys if the request failed with 5xx error or network error. default is 0
@@ -33,45 +34,48 @@ module.exports = function configJSON(req) {
         // wait in ms between retry.
         retryDelay: 1000,
         // The number of concurrent requests Journey Builder will send all together
-        concurrentRequests: 5
-      }
+        concurrentRequests: 5,
+      },
     },
     configurationArguments: {
+      applicationExtensionKey: '4b50336f-01b9-4d0b-bd3a-731cc2038027',
       publish: {
-        url: `https://${req.headers.host}/modules/discount-code/publish`
+        url: `https://custom-activity-demo-0b2f5160126d.herokuapp.com/modules/discount-code/publish`,
       },
       validate: {
-        url: `https://${req.headers.host}/modules/discount-code/validate`
+        url: `https://custom-activity-demo-0b2f5160126d.herokuapp.com/modules/discount-code/validate`,
       },
       stop: {
-        url: `https://${req.headers.host}/modules/discount-code/stop`
-      }
+        url: `https://custom-activity-demo-0b2f5160126d.herokuapp.com/modules/discount-code/stop`,
+      },
     },
     userInterfaces: {
-      configurationSupportsReadOnlyMode : true,
+      configurationSupportsReadOnlyMode: true,
       configInspector: {
         size: 'scm-lg',
-        emptyIframe: true
-      }
+        emptyIframe: true,
+      },
     },
     schema: {
       arguments: {
         execute: {
           inArguments: [],
-          outArguments: [{
-            discountCode: {
-              dataType: 'Text',
-              direction: 'out',
-              access: 'visible'
+          outArguments: [
+            {
+              discountCode: {
+                dataType: 'Text',
+                direction: 'out',
+                access: 'visible',
+              },
+              discount: {
+                dataType: 'Number',
+                direction: 'out',
+                access: 'visible',
+              },
             },
-            discount: {
-              dataType: 'Number',
-              direction: 'out',
-              access: 'visible'
-            }
-          }]
-        }
-      }
-    }
+          ],
+        },
+      },
+    },
   };
 };
--- modules/discount-redemption-split/config/config-json.js
+++ modules/discount-redemption-split/config/config-json.js
@@ -5,7 +5,7 @@ module.exports = function configJSON(req) {
     metaData: {
       // the location of our icon file
       icon: `images/icon.svg`,
-      category: 'customer'
+      category: 'customer',
     },
     // For Custom Activity this must say, "REST"
     type: 'RESTDECISION',
@@ -13,94 +13,100 @@ module.exports = function configJSON(req) {
       // Internationalize your language here!
       'en-US': {
         name: 'Code Engagement',
-        description: 'Check the status of the discount code.'
-      }
+        description: 'Check the status of the discount code.',
+      },
     },
     arguments: {
       execute: {
-        inArguments: [{
-          discountCode: "{{Interaction.discountCode}}"
-        }],
+        inArguments: [
+          {
+            discountCode: '{{Interaction.discountCode}}',
+          },
+        ],
         outArguments: [],
         // Fill in the host with the host that this is running on.
         // It must run under HTTPS
-        url: `https://${req.headers.host}/modules/discount-redemption-split/execute`
-      }
+        url: `https://custom-activity-demo-0b2f5160126d.herokuapp.com/modules/discount-redemption-split/execute`,
+      },
     },
     configurationArguments: {
+      applicationExtensionKey: '4b50336f-01b9-4d0b-bd3a-731cc2038027',
       save: {
-        url: `https://${req.headers.host}/modules/discount-redemption-split/save`
+        url: `https://custom-activity-demo-0b2f5160126d.herokuapp.com/modules/discount-redemption-split/save`,
       },
       publish: {
-        url: `https://${req.headers.host}/modules/discount-redemption-split/publish`
+        url: `https://custom-activity-demo-0b2f5160126d.herokuapp.com/modules/discount-redemption-split/publish`,
       },
       validate: {
-        url: `https://${req.headers.host}/modules/discount-redemption-split/validate`
+        url: `https://custom-activity-demo-0b2f5160126d.herokuapp.com/modules/discount-redemption-split/validate`,
       },
       stop: {
-        url: `https://${req.headers.host}/modules/discount-redemption-split/stop`
-      }
+        url: `https://custom-activity-demo-0b2f5160126d.herokuapp.com/modules/discount-redemption-split/stop`,
+      },
     },
     userInterfaces: {
       configInspector: {
         size: 'scm-lg',
         emptyIframe: true,
-      }
+      },
     },
-    outcomes: [{
+    outcomes: [
+      {
         arguments: {
-          branchResult: 'no_activity'
+          branchResult: 'no_activity',
         },
         metaData: {
-          label: 'No Activity'
-        }
+          label: 'No Activity',
+        },
       },
       {
         arguments: {
-          branchResult: 'viewed_item'
+          branchResult: 'viewed_item',
         },
         metaData: {
-          label: 'Viewed Item'
-        }
+          label: 'Viewed Item',
+        },
       },
       {
         arguments: {
-          branchResult: 'abandoned_cart'
+          branchResult: 'abandoned_cart',
         },
         metaData: {
-          label: 'Abandoned Cart'
-        }
+          label: 'Abandoned Cart',
+        },
       },
       {
         arguments: {
-          branchResult: 'purchased_item'
+          branchResult: 'purchased_item',
         },
         metaData: {
-          label: 'Purchased Item'
-        }
+          label: 'Purchased Item',
+        },
       },
       {
         arguments: {
-          branchResult: 'invalid_code'
+          branchResult: 'invalid_code',
         },
         metaData: {
-          label: 'Invalid Code'
-        }
-      }
+          label: 'Invalid Code',
+        },
+      },
     ],
     schema: {
       arguments: {
         execute: {
-          inArguments: [{
-            discountCode: {
-              dataType: 'Text',
-              direction: 'in',
-              access: 'visible'
-            }
-          }],
-          outArguments: []
-        }
-      }
-    }
+          inArguments: [
+            {
+              discountCode: {
+                dataType: 'Text',
+                direction: 'in',
+                access: 'visible',
+              },
+            },
+          ],
+          outArguments: [],
+        },
+      },
+    },
   };
 };
